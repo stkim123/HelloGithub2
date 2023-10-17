c879a7ec1a72d9c9faba293242a0057597b14e2a
bongOmoon <ansqhddh@gmail.com>
2023-10-17 10:37:17 +0900

    [Add Feature] 1. Add do111111111 method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public void do11111111111()
+    {
+        System.out.println("111111111111111111111111111");
+    }
 }
